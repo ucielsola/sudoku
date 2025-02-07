13719941ad7b73a90519951fd2b0124318d7e6e2
Uciel Sola <solauciel@gmail.com>
2025-02-06 23:39:29 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+	plugins: [sveltekit(), tailwindcss()],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
+
